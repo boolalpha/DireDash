2de3671f779d2c7812fe457886da10cc90399f8c
IggyWright <iggy.wright81@gmail.com>
2022-07-30 12:25:30 -0400

    remove google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-8C8432L33P"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+// import ReactGA from 'react-ga';
+// const TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS; // OUR_TRACKING_ID
+// ReactGA.initialize(TRACKING_ID);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
